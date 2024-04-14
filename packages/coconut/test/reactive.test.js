60cf4c9367d7cf189ebcc14d6085578e4a6726ef
jcg01423690 <jcg01423690@alibaba-inc.com>
2024-04-14 14:36:47 +0800

    feat: 添加jest

---
+++ packages/coconut/test/reactive.test.js
@@ -0,0 +1,15 @@
+import { _factory } from '../src';
+
+describe("reactive", () => {
+  test("修改值，则支持cb", () => {
+    const obj = { age: 22 };
+    const cb = jest.fn();
+    const decor = _factory(cb);
+    const initFns = [];
+    decor(undefined, { kind: 'field', name: 'age', addInitializer: (fn) => initFns.push(fn)});
+    initFns.map(fn => fn.call(obj));
+    obj.age = 23
+    expect(cb).toHaveBeenCalledTimes(1);
+    expect(obj.age).toBe(23);
+  })
+})
