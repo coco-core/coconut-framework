01825a6e922c45e4b7eaa6dc394d9c03513a284b
jcg01423690 <jcg01423690@alibaba-inc.com>
2024-08-06 23:47:59 +0800

    feat: 用html-webpack-plugin替代index.html

--- packages/demo/webpack.base.config.js
+++ packages/demo/webpack.base.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { InjectBodyPlugin } = require("inject-body-webpack-plugin");
 
 module.exports = {
   entry: './src/.coco/index.jsx',
@@ -20,4 +22,16 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent: `
+    <html>
+    <head><meta charset="UTF-8"></head>
+      <body>
+        <div id="root"></div>
+      </body>
+    </html>
+  `
+    })
+  ],
 };
