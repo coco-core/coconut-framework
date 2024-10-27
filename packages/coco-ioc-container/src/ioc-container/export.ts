d994e1a4ee5aff574636658c7040d4e8f7021b67
onfocus <jiang.chenguang@outlook.com>
2024-10-27 09:52:54 +0800

    refactor: 删除export.ts文件

--- packages/coco-ioc-container/src/ioc-container/export.ts
+++
@@ -1,4 +0,0 @@
-
-export { associateClassMetadata, associateFieldMetadata, getClsMetadata, getFields, clear } from "./metadata.ts";
-export { getBean, getDefinition } from './bean-factory'
-export { default as ApplicationContext } from "./application-context.ts"
