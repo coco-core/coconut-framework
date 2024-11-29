4adef8bed1d1a8cba1ae977a7df3e42309395119
jcg <jiang.chenguang@outlook.com>
2024-11-29 13:27:45 +0800

    refactor: reactive -> memoized

--- packages/coco-reactive/src/decorator/reactive.ts
+++ packages/coco-reactive/src/decorator/reactive.ts
@@ -7,8 +7,8 @@ import {
   type ApplicationContext,
   type FieldContext,
 } from 'coco-ioc-container';
-import Publisher from '../reactive/publisher.ts';
-import Subscriber from '../reactive/subscriber.ts';
+import Publisher from '../memoized/publisher.ts';
+import Subscriber from '../memoized/subscriber.ts';
 
 @target([Target.Type.Field])
 export class Reactive extends Metadata {}
