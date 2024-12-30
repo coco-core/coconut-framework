cdba361ba8dca4e5950b00cd8bc149b7c6dd87c5
jcg <jiang.chenguang@outlook.com>
2024-12-30 20:39:00 +0800

    feat: updateVersion也同时更新template下的版本

--- scripts/publish/update-version.js
+++ scripts/publish/update-version.js
@@ -8,7 +8,8 @@ function prefixZero(num) {
 
 function updateVersion() {
   const now = new Date();
-  const newVersion = `${now.getFullYear()}${now.getMonth() + 1}${prefixZero(now.getDate())}${prefixZero(now.getHours())}${prefixZero(now.getMinutes())}`;
+  const timestamp = `${now.getFullYear()}${now.getMonth() + 1}${prefixZero(now.getDate())}${prefixZero(now.getHours())}${prefixZero(now.getMinutes())}`;
+  let newVersion = '';
   const packages = ['packages/coco-mvc', 'packages/coco-cli'];
   packages.forEach((pkg) => {
     const packageJson = path.join(process.cwd(), pkg, 'package.json');
@@ -16,12 +17,31 @@ function updateVersion() {
       if (err) console.error(err);
       else {
         // 目前只更新最后的数字，表示发版的日期小时分钟。前面的版本号手动更新
-        content.version = `${content.version.slice(0, -newVersion.length)}${newVersion}`;
+        if (!newVersion) {
+          newVersion = `${content.version.slice(0, -timestamp.length)}${timestamp}`;
+        }
+        content.version = newVersion;
         jsonfile.writeFile(packageJson, content, { spaces: 2 }, (err) => {
           if (err) console.error(err);
         });
       }
     });
+  });
+
+  // template/package.json
+  const packageJson = path.join(
+    process.cwd(),
+    'packages/coco-cli/template/package.json'
+  );
+  jsonfile.readFile(packageJson, (err, content) => {
+    if (err) console.error(err);
+    else {
+      content.dependencies['coco-mvc'] = `^${newVersion}`;
+      content.devDependencies['@cocofw/cli'] = `^${newVersion}`;
+      jsonfile.writeFile(packageJson, content, { spaces: 2 }, (err) => {
+        if (err) console.error(err);
+      });
+    }
   });
 }
 
