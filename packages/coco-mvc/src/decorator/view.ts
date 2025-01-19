1ec45b3b1148009e71b127d0b157ab87c75f5276
jcg <jiang.chenguang@outlook.com>
2025-01-19 09:31:27 +0800

    feat: view装饰器类型添加ClassDecoratorContext

--- packages/coco-mvc/src/decorator/view.ts
+++ packages/coco-mvc/src/decorator/view.ts
@@ -1,6 +1,9 @@
 import { genDecorator } from 'coco-ioc-container';
 import View from '../metadata/view.ts';
 
-export default genDecorator<string, ClassMethodDecoratorContext>(View, {
+export default genDecorator<
+  string,
+  ClassDecoratorContext | ClassMethodDecoratorContext
+>(View, {
   optional: true,
 });
