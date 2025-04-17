50e3d9f82ff603d5796c6d364c628d55b1db98bb
jcg <jiang.chenguang@outlook.com>
2025-04-17 22:25:05 +0800

    feat: 优化解析命令的方式

--- packages/coco-cli/src/build-lib.ts
+++ packages/coco-cli/src/build-lib.ts
@@ -3,16 +3,16 @@ import fs from 'node:fs';
 import rollup from 'rollup';
 import { execSync, spawn } from 'child_process';
 import process from 'node:process';
-import { resolveFromProject } from './util/resolve';
+import { resolveCli } from './util/resolve';
 
 export const build = async () => {
-  const rollupJs = resolveFromProject('rollup');
+  const rollup = resolveCli('rollup');
   const configPath = path.resolve(
     __dirname,
     '../build-config/lib/rollup.config.js'
   );
-  const args = [rollupJs, '--config', configPath];
-  return spawn('node', args, {
+  const args = ['--config', configPath];
+  return spawn(rollup, args, {
     cwd: process.cwd(),
     stdio: 'inherit',
   });
